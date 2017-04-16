6ae11bab6e61fabb01791281ade84d169c5e7b21
w0282 <wuhj@dtdream.com>
2017-04-17 07:48:54 +0800

    demo2,多个入口文件.

---
+++ demo02/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: ['./hello.js', './world.js'],
+    output: {
+        path: path.resolve(__dirname, './'),
+        filename: 'bundle.js'
+    }
+
+}
