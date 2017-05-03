684903e4bd42ef9977ee2d7fb516fbd5ff89ee2e
w0282 <wuhj@dtdream.com>
2017-05-03 10:53:06 +0800

    增加sass-loader.

--- demo04/main.js
+++ demo04/main.js
@@ -1,2 +1,3 @@
 require('./app.css');
+require('./test.scss');
 
--- demo04/webpack.config.js
+++ demo04/webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     }
