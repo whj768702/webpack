ff033398d797394f988fc1bb790558fbe5c6a18f
whj768702 <whj768702@gmail.com>
2022-04-26 19:05:13 +0800

    feat(webpack): babel-loader @babel/preset-env作用

--- demo03/webpack.config.js
+++ demo03/webpack.config.js
@@ -1,23 +1,35 @@
 const path = require('path');
 
 module.exports = {
-    entry: './main.js',
-    output: {
-        path: path.resolve(__dirname, './'),
-        filename: 'bundle.js'
-    },
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, './'),
+    filename: 'bundle.js',
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: { presets: ['es2015'] }
-                }
-            }
-        ]
-    }
-
-}
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          // options中的可以放到.babelrc配置文件中,效果一样
+          //   options: {
+          //     presets: [
+          //       [
+          //         '@babel/preset-env',
+          //         {
+          //           targets: {
+          //             chrome: '100',
+          //             // esmodules: true,
+          //           },
+          //         },
+          //       ],
+          //     ],
+          //   },
+        },
+      },
+    ],
+  },
+};
